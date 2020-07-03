f44815b4ea0d9c34b28df905be515da4d0d56d8e
unknown <andersonlantunes@gmail.com>
2020-07-03 20:25:24 -0300

    Segunda parte validação

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
-const AddInputBlurEvent = () => {};
+const AddInputFocusEvent = () => {
+  var inputList = document.querySelectorAll(".form-group input");
+  inputList.forEach((input) => {
+    let inputId = document.querySelector(`#${input.id}`);
+    inputId.addEventListener("focus", (event) => {
+      let siblingId = event.target.nextElementSibling.id;
+      let sibling = document.querySelector(`#${siblingId}`);
+
+      let elementId = event.target.id;
+      let element = document.querySelector(`#${elementId}`);
+
+      element.style.borderColor = "unset;";
+      sibling.innerHTML = "";
+    });
+  });
+};
 
 const FormValidate = () => {
   let error = false;
 
   if (!codigo.value) {
-    codigo.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher o nome.';
+    codigo.style.border = "3px solid red";
+    codigoValida.innerHTML = "Favor preencher o nome.";
   } else {
     codigo.style.borderColor = "unset";
     error = true;
   }
 
   if (!nome.value) {
-    nome.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher o nome.';
+    nome.style.borderColor = "3px solid red";
+    nomeValida.innerHTML = "Favor preencher o nome.";
   } else {
     nome.style.borderColor = "unset";
     error = true;
   }
 
   if (!cep.value) {
-    cep.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher o CEP.';
+    cep.style.borderColor = "3px solid red";
+    cepValida.innerHTML = "Favor preencher o CEP.";
   } else {
     cep.style.borderColor = "unset";
     error = true;
   }
 
   if (!rua.value) {
-    rua.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher a rua.';
+    rua.style.borderColor = "3px solid red";
+    ruaValida.innerHTML = "Favor preencher a rua.";
   } else {
     rua.style.borderColor = "unset";
     error = true;
   }
 
   if (!numero.value) {
-    numero.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher o número.';
+    numero.style.borderColor = "3px solid red";
+    numeroValida.innerHTML = "Favor preencher o número.";
   } else {
     numero.style.borderColor = "unset";
     error = true;
   }
 
   if (!bairro.value) {
-    bairro.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher o bairro.';
+    bairro.style.borderColor = "3px solid red";
+    bairroValida.innerHTML = "Favor preencher o bairro.";
   } else {
     bairro.style.borderColor = "unset";
     error = true;
   }
 
   if (!cidade.value) {
-    cidade.style.borderColor = "red";
-    // id.innerHTML = 'Favor preencher a cidade.';
+    cidade.style.borderColor = "3px solid red";
+    cidadeValida.innerHTML = "Favor preencher a cidade.";
   } else {
     cidade.style.borderColor = "unset";
     error = true;
@@ -61,7 +76,9 @@ const FormValidate = () => {
 };
 
 btnEnviar.addEventListener("click", () => {
-  if (!FormValidate()) {
+  if (FormValidate()) {
     console.log("Validado!");
   }
 });
+
+AddInputFocusEvent();
